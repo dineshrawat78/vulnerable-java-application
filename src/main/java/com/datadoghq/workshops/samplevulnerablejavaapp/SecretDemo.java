468c80a387cb68a7fe7e6f6dce6a7d1b2dd63066
dineshrawat78 <103558046+dineshrawat78@users.noreply.github.com>
2025-10-23 00:54:48 +0530

    Update SecretDemo.java

--- src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/SecretDemo.java
+++ src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/SecretDemo.java
@@ -44,12 +44,14 @@ public class SecretDemo {
         // Hard-coded secret placeholder
         String hardCodedApiKey = "<HARD_CODED_SECRET>"; // <-- placeholder only!
 
-        String password = "test@1234"; // <-- placeholder only!
+        String password = "test@1234sdfasf"; // <-- placeholder only!
 
         String apiKey = "sec1-test1234ugugugiugtiutuytuyfuyhfghjfjhgjhgjhghjghjkhjkhjkhkjy"; // <-- placeholder only!
 
         // Insecure: logging the secret directly (for demonstration only)
         System.out.println("Insecure log: using API key = " + hardCodedApiKey);
+        System.out.println("Insecure log: using API key = " + apiKey);
+         System.out.println("Insecure log: using password = " + password);
 
         // Simulate using the secret
         boolean ok = callExternalServiceWithKey(hardCodedApiKey);
